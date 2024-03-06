1e1afbc6ab08a33d8a5c6dfc7cb29d68ff5e6772
nedrin1 <aachinyaev@yandex.ru>
2024-03-06 09:11:19 +0700

    change code

---
+++ patch1/hello_world.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+    char* username;
+    std::cout << "Enter user name\n";
+    std::cin << username;
+    std::cout << "Hello world from " << username << std::endl;
+}
